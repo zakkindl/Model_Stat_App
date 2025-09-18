eb7b948f9339c98b130d34fd4d97c2cb138d4eb3
zakkindl <zakgkindl@gmail.com>
2025-09-17 20:58:49 -0700

    Calculated model statistic values and added plotly plot

--- Model_Stat/app.R
+++ Model_Stat/app.R
@@ -1,5 +1,5 @@
 # Load packages
-packages <- c('shiny','tidyverse','googlesheets4','shinycssloaders','DT')
+packages <- c('shiny','tidyverse','googlesheets4','shinycssloaders','DT','plotly')
 
 lapply(packages, library, character.only = TRUE)
 
@@ -41,11 +41,16 @@ ui <- fluidPage(
  #### Main Panel ####   
     mainPanel(
       
-      h3("Filtered Data"),
-              DT::dataTableOutput("data_table"),
+      plotlyOutput("plotly_plot")
       
-      h3("Filtered Data"),
-      DT::dataTableOutput("data_table_2")
+      # h3("First Week"),
+      #         DT::dataTableOutput("data_table"),
+      # 
+      # h3("Second Week"),
+      # DT::dataTableOutput("data_table_2"),
+      # 
+      # h3("Compared Week"),
+      # DT::dataTableOutput("comparison_data")
       
       # Add your outputs here
       
@@ -130,7 +135,7 @@ server <- function(input, output, session) {
     
   })
   
-  # Weeks Select Input based on season category selected
+  # Weeks filtered based on season selected
   observe({
     req(filtered_data())  
     
@@ -146,7 +151,7 @@ server <- function(input, output, session) {
                       choices = available_weeks)
   })
   
-  # Filtered data for comparison weeks selected
+  # Filtered data for comparing weeks selected
   table_data <- reactive({
     req(sheet_data(), input$category_select, input$year_select, 
         input$week_one_select, input$week_two_select)
@@ -158,8 +163,8 @@ server <- function(input, output, session) {
              week >= min(input$week_one_select, input$week_two_select),
              week <= max(input$week_one_select, input$week_two_select))
   })
-
   
+#Timepoint 1
   timepoint_1 <- reactive({
     req(table_data(), input$week_one_select)
     
@@ -169,12 +174,22 @@ server <- function(input, output, session) {
       select(athletes, date, assessment, metric, value, week) |>
       group_by(metric, date) |>  
       summarise(mean_value = mean(value, na.rm = TRUE),
+                sd_value = sd(value,na.rm = TRUE),
                 athletes = first(athletes),
                 assessment = first(assessment),
                 week = first(week),
                 .groups = 'drop') 
   })
   
+  # Data table output 1
+  output$data_table <- DT::renderDataTable({
+    timepoint_1()
+  }, options = list(
+    pageLength = 10,
+    scrollX = TRUE
+  ))
+  
+#Timepoint 2
   timepoint_2 <- reactive({
     req(table_data(), input$week_two_select)
     
@@ -184,27 +199,91 @@ server <- function(input, output, session) {
       select(athletes, date, assessment, metric, value, week) |>
       group_by(metric, date) |>  
       summarise(mean_value = mean(value, na.rm = TRUE),
+                sd_value = sd(value,na.rm = TRUE),
                 athletes = first(athletes),
                 assessment = first(assessment),
                 week = first(week),
                 .groups = 'drop')
   })
   
-  # Data table output
-  output$data_table <- DT::renderDataTable({
-    timepoint_2()
-  }, options = list(
-    pageLength = 10,
-    scrollX = TRUE
-  ))
-  
-  # Data table output
+
+  # Data table output 2
   output$data_table_2 <- DT::renderDataTable({
     timepoint_2()
   }, options = list(
     pageLength = 10,
     scrollX = TRUE
   ))
+  
+  
+#Comparison Data
+  comparison_data <- reactive({
+    req(timepoint_1(), timepoint_2())
+    
+    # Join the two datasets by metric
+    timepoint_1() |>
+      select(metric, mean_value,sd_value, week, date) |>
+      rename(mean_t1 = mean_value,
+             sd_t1 = sd_value,
+             week_t1 = week, 
+             date_t1 = date) |>
+      full_join(
+        timepoint_2() |>
+          select(metric, mean_value,sd_value, week, date) |>
+          rename(mean_t2 = mean_value,
+                 sd_t2 = sd_value,
+                 week_t2 = week,
+                 date_t2 = date),
+        by = "metric"
+      ) |>
+      group_by(metric) |> 
+      summarise(
+        abs_mean_diff = first(abs(mean_t1 - mean_t2)),
+        sd_mean = first(((sd_t1^2 + sd_t2^2)/2)^.5),
+        percent_change = first(((mean_t2 - mean_t1) / mean_t1) * 100),
+        critical_diff = first(sd_mean*1.3733),
+        significance = case_when(abs_mean_diff > critical_diff ~ "significant_diff",
+                                 abs_mean_diff <= critical_diff ~ "non-significant_diff"),
+      .groups = 'drop')
+  })
+  
+  output$comparison_data <- DT::renderDataTable({
+    comparison_data()
+  }, options = list(
+    pageLength = 10,
+    scrollX = TRUE
+  ))
+  
+  output$plotly_plot <- renderPlotly({
+    req(comparison_data())
+    
+    plot_data <- comparison_data() %>%
+      mutate(color_group = case_when(
+        significance == "non-significant_diff" ~ "Non-Significant",
+        significance == "significant_diff" & percent_change < 0 ~ "Significant Decrease",
+        significance == "significant_diff" & percent_change > 0 ~ "Significant Increase",
+        TRUE ~ "Non-significant"
+      ))
+    
+    p <- plot_data %>%
+      plot_ly(x = ~metric, 
+              y = ~percent_change,
+              color = ~color_group,
+              colors = c("Non-Significant" = "grey", 
+                         "Significant Decrease" = "red", 
+                         "Significant Increase" = "darkgreen"),
+              type = "bar",
+              text = ~paste("<br>Change:", round(percent_change, 2), "%")) %>%
+      layout(title = paste0("CMJ Metrics Week ",input$week_one_select, " to Week ", input$week_two_select),
+             xaxis = list(title = ""),
+             yaxis = list(title = "Percent Change (%)"),
+             legend = list(title = "Change Type"),
+             font = list(family = "Helvetica Neue"))
+    p
+    
+    
+  })
+  
   
   
 }
